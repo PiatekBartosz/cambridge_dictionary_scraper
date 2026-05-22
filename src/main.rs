6ae4665ed22ca1af4345637514398c6b08e40d9b
Bartosz Piątek <bartekpiatek123@gmail.com>
2026-05-23 00:56:31 +0200

    Get query from CLI / interactively

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+use dialoguer::Input;
+use dialoguer::theme::ColorfulTheme;
+
+#[derive(Parser)]
+#[command(
+    version,
+    name = "cam",
+    about = "CLI for web scraping Cambridge Dictionary Word definitions"
+)]
+struct Args {
+    /// Word that will have definition checked
+    query: Option<String>,
+}
+
+fn get_query(args: Args) -> String {
+    let theme = ColorfulTheme {
+        prompt_suffix: console::style("".to_string()).for_stderr(),
+        ..ColorfulTheme::default()
+    };
+
+    match args.query {
+        Some(query) => query,
+        None => Input::<String>::with_theme(&theme)
+            .with_prompt(">")
+            .interact_text()
+            .unwrap(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let query = get_query(args);
+
+    println!("Hello, world! {}", query);
 }
